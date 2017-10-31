2b1f091c2b676b8264dc26c02b9b0d86fd62b89f
Kuku Teslim <teslim.kuku@accessbankplc.com>
2017-10-31 17:19:22 +0100

    Android Services sample code

---
+++ src/AndroidServices/app/src/main/java/com/techdroit/androidservices/MainActivity.java
@@ -0,0 +1,31 @@
+package com.techdroit.androidservices;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
